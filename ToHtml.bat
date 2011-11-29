143309f88d60fd3b426cc84d3518c76ab2e8a33c
Arlo Belshee <arlo.belshee@microsoft.com>
2011-11-29 11:44:52 -0800

    Created the documents to match Mike & Mike's proposed ToC. They're still blanks, but they build correctly.

--- ToHtml.bat
+++ ToHtml.bat
@@ -1,4 +1,10 @@
 rmdir /s/q output
 mkdir output
-Pandoc\bin\pandoc.exe --from markdown --to html -o output\spec.html *.markdown
+
+Pandoc\bin\pandoc.exe --from markdown --to html -o output\OData.html "OData.markdown"
+Pandoc\bin\pandoc.exe --from markdown --to html -o output\OData_Format_AtomPub.html "OData AtomPub Format.markdown"
+Pandoc\bin\pandoc.exe --from markdown --to html -o output\OData_Format_Batch.html "OData Batch Processing Format.markdown"
+Pandoc\bin\pandoc.exe --from markdown --to html -o output\OData_Format_Json.html "OData Json Format.markdown"
+Pandoc\bin\pandoc.exe --from markdown --to html -o output\OData_URI_Conventions.html "OData URI Conventions.markdown"
+
 rem pause
--- ToRtf.bat
+++ ToRtf.bat
@@ -1,4 +1,10 @@
 rmdir /s/q output
 mkdir output
-Pandoc\bin\pandoc.exe --from markdown -o output\spec.rtf -s *.markdown
+
+Pandoc\bin\pandoc.exe --from markdown -o output\OData.rtf -s "OData.markdown"
+Pandoc\bin\pandoc.exe --from markdown -o output\OData_Format_AtomPub.rtf "OData AtomPub Format.markdown"
+Pandoc\bin\pandoc.exe --from markdown -o output\OData_Format_Batch.rtf "OData Batch Processing Format.markdown"
+Pandoc\bin\pandoc.exe --from markdown -o output\OData_Format_Json.rtf "OData Json Format.markdown"
+Pandoc\bin\pandoc.exe --from markdown -o output\OData_URI_Conventions.rtf "OData URI Conventions.markdown"
+
 rem pause

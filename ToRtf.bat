b775b3b7ee8cb98cbfd9dd76fff50f5cb5b96525
Arlo Belshee <arlo.belshee@microsoft.com>
2012-01-19 10:51:17 -0800

    Switching over to multimarkdown.

--- ToRtf.bat
+++ ToRtf.bat
@@ -1,10 +1,7 @@
 rmdir /s/q output
 mkdir output
 
-Pandoc\bin\pandoc.exe --from markdown -o output\OData.rtf -s "OData.markdown"
-Pandoc\bin\pandoc.exe --from markdown -o output\OData_Format_AtomPub.rtf "OData AtomPub Format.markdown"
-Pandoc\bin\pandoc.exe --from markdown -o output\OData_Format_Batch.rtf "OData Batch Processing Format.markdown"
-Pandoc\bin\pandoc.exe --from markdown -o output\OData_Format_Json.rtf "OData Json Format.markdown"
-Pandoc\bin\pandoc.exe --from markdown -o output\OData_URI_Conventions.rtf "OData URI Conventions.markdown"
+multimarkdown\multimarkdown.exe -b -t odf *.markdown
+move *.fodt output
 
 rem pause
